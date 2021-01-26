528be03cb0240be17c87420fdb4f60b5d2fd00f7
Ashutosh Tiwari <findashutoshtiwari@gmail.com>
2021-01-26 21:22:12 +0530

    using universal path so can be used from different machines as well

--- RFSC_audio_detection/run.py
+++ RFSC_audio_detection/run.py
@@ -14,21 +14,22 @@ import models
 import losses
 import functions
 import augmentations
+from tqdm import tqdm
 
-class args:
+class Args:
     DEBUG = True
     exp_name = "resnest50d_5fold_base"
     network = "AudioClassifier"
     pretrain_weights = None
     model_param = {
-        'encoder' : 'resnest50d',
+        'encoder': 'resnest50d',
         'sample_rate': 48000,
-        'window_size' : 512 * 2,
-        'hop_size' : 345 * 2,
-        'mel_bins' : 128,
-        'fmin' : 20,
-        'fmax' : 48000 // 2,
-        'classes_num' : 24
+        'window_size': 512 * 2,
+        'hop_size': 345 * 2,
+        'mel_bins': 128,
+        'fmin': 20,
+        'fmax': 48000 // 2,
+        'classes_num': 24
     }
     losses = "BCEWithLogitsLoss"
     lr = 1e-3
@@ -43,14 +44,14 @@ class args:
     early_stop = 10
 
     device = ('cuda' if torch.cuda.is_available() else 'cpu')
-    train_csv = "train_folds.csv"
+    train_csv = "/home/ashutosh/code/kaggle/RFSC_audio_detection/train_folds.csv"
     test_csv = "test_df.csv"
-    sub_csv = "data/sample_submission.csv"
+    sub_csv = "/home/ashutosh/code/kaggle/RFSC_audio_detection/data/sample_submission.csv"
     output_dir = "weights"
 
 
 def main(fold):
-    seed = args.seed
+    seed = Args.seed
     random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
     np.random.seed(seed)
@@ -58,31 +59,31 @@ def main(fold):
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
 
-    args.fold = fold
-    args.save_path = os.path.join(args.output_dir, args.exp_name)
-    os.makedirs(args.save_path, exist_ok=True)
+    Args.fold = fold
+    Args.save_path = os.path.join(Args.output_dir, Args.exp_name)
+    os.makedirs(Args.save_path, exist_ok=True)
 
-    train_df = pd.read_csv(args.train_csv)
+    train_df = pd.read_csv(Args.train_csv)
 
-    sub_df = pd.read_csv(args.sub_csv)
-    if args.DEBUG:
+    sub_df = pd.read_csv(Args.sub_csv)
+    if Args.DEBUG:
         train_df = train_df.sample(100)
         sub_df = sub_df.sample(10)
-        args.batch_size = 4
-        args.epochs = 5
+        Args.batch_size = 4
+        Args.epochs = 5
     train_fold = train_df[train_df.kfold != fold]
     valid_fold = train_df[train_df.kfold == fold]
 
     train_dataset = datasets.AudioDataset(
         df=train_fold,
-        period=args.period,
+        period=Args.period,
         transforms=augmentations.augmenter,
         train=True,
         data_path="data/train"
     )
     valid_dataset = datasets.AudioDataset(
         df=valid_fold,
-        period=args.period,
+        period=Args.period,
         transforms=None,
         train=True,
         data_path="data/train"
@@ -90,7 +91,7 @@ def main(fold):
 
     test_dataset = datasets.TestDataset(
         df=sub_df,
-        period=args.period,
+        period=Args.period,
         transforms=None,
         train=False,
         data_path="data/test"
@@ -98,71 +99,71 @@ def main(fold):
 
     train_loader = torch.utils.data.DataLoader(
         train_dataset,
-        batch_size=args.batch_size,
+        batch_size=Args.batch_size,
         shuffle=True,
         drop_last=True,
-        num_workers=args.num_workers
+        num_workers=Args.num_workers
     )
     valid_loader = torch.utils.data.DataLoader(
         valid_dataset,
-        batch_size=args.batch_size,
+        batch_size=Args.batch_size,
         shuffle=False,
         drop_last=False,
-        num_workers=args.num_workers
+        num_workers=Args.num_workers
     )
 
     test_loader = torch.utils.data.DataLoader(
         test_dataset,
-        batch_size=args.batch_size//2,
+        batch_size=Args.batch_size//2,
         shuffle=False,
         drop_last=False,
-        num_workers=args.num_workers
+        num_workers=Args.num_workers
     )
 
-    model = models.__dict__[args.network](**args.model_param)
-    model = model.to(args.device)
+    model = models.__dict__[Args.network](**Args.model_param)
+    model = model.to(Args.device)
 
-    if args.pretrain_weights:
+    if Args.pretrain_weights:
         print("---------------------loading pretrain weights")
-        model.load_state_dict(torch.load(args.pretrain_weights, map_location=args.device)["model"], strict=False)
-        model = model.to(args.device)
+        model.load_state_dict(torch.load(Args.pretrain_weights, map_location=Args.device)["model"], strict=False)
+        model = model.to(Args.device)
 
-    criterion = losses.__dict__[args.losses]()
-    optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr)
-    num_train_steps = int(len(train_loader) * args.epochs)
-    num_warmup_steps = int(0.1 * args.epochs * len(train_loader))
+    criterion = losses.__dict__[Args.losses]()
+    optimizer = torch.optim.AdamW(model.parameters(), lr=Args.lr)
+    num_train_steps = int(len(train_loader) * Args.epochs)
+    num_warmup_steps = int(0.1 * Args.epochs * len(train_loader))
     scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=num_warmup_steps, num_training_steps=num_train_steps)
 
     best_lwlrap = -np.inf
-    for epoch in range(args.start_epoch, args.epochs):
-        train_avg, train_loss = functions.train_epoch(args, model, train_loader, criterion, optimizer, scheduler, epoch)
-        valid_avg, valid_loss = functions.valid_epoch(args, model, valid_loader, criterion, epoch)
+    for epoch in range(Args.start_epoch, Args.epochs):
+        train_avg, train_loss = functions.train_epoch(Args, model, train_loader, criterion, optimizer, scheduler, epoch)
+        valid_avg, valid_loss = functions.valid_epoch(Args, model, valid_loader, criterion, epoch)
 
-        if args.epoch_scheduler:
+        if Args.epoch_scheduler:
             scheduler.step()
 
         content = f"""
                 {time.ctime()} \n
-                Fold:{args.fold}, Epoch:{epoch}, lr:{optimizer.param_groups[0]['lr']:.7}\n
+                Fold:{Args.fold}, Epoch:{epoch}, lr:{optimizer.param_groups[0]['lr']:.7}\n
                 Train Loss:{train_loss:0.4f} - LWLRAP:{train_avg['lwlrap']:0.4f}\n
                 Valid Loss:{valid_loss:0.4f} - LWLRAP:{valid_avg['lwlrap']:0.4f}\n
         """
         print(content)
-        with open(f'{args.save_path}/log_{args.exp_name}.txt', 'a') as appender:
+        with open(f'{Args.save_path}/log_{Args.exp_name}.txt', 'a') as appender:
             appender.write(content+'\n')
 
         if valid_avg['lwlrap'] > best_lwlrap:
             print(f"########## >>>>>>>> Model Improved From {best_lwlrap} ----> {valid_avg['lwlrap']}")
-            torch.save(model.state_dict(), os.path.join(args.save_path, f'fold-{args.fold}.bin'))
+            torch.save(model.state_dict(), os.path.join(Args.save_path, f'fold-{Args.fold}.bin'))
             best_lwlrap = valid_avg['lwlrap']
         #torch.save(model.state_dict(), os.path.join(args.save_path, f'fold-{args.fold}_last.bin'))
 
 
-    model.load_state_dict(torch.load(os.path.join(args.save_path, f'fold-{args.fold}.bin'), map_location=args.device))
-    model = model.to(args.device)
+    model.load_state_dict(torch.load(os.path.join(Args.save_path, f'fold-{Args.fold}.bin'), map_location=Args.device))
+    model = model.to(Args.device)
 
     target_cols = sub_df.columns[1:].values.tolist()
-    test_pred, ids = functions.test_epoch(args, model, test_loader)
+    test_pred, ids = functions.test_epoch(Args, model, test_loader)
     print(np.array(test_pred).shape)
 
     test_pred_df = pd.DataFrame({
@@ -173,8 +174,8 @@ def main(fold):
         assert len(target_cols) == test_pred.shape[1]
         test_pred_df[target_cols[col]] = test_pred[:, col]
 
-    test_pred_df.to_csv(os.path.join(args.save_path, f"fold-{args.fold}-submission.csv"), index=False)
-    print(os.path.join(args.save_path, f"fold-{args.fold}-submission.csv"))
+    test_pred_df.to_csv(os.path.join(Args.save_path, f"fold-{Args.fold}-submission.csv"), index=False)
+    print(os.path.join(Args.save_path, f"fold-{Args.fold}-submission.csv"))
 
 
 
